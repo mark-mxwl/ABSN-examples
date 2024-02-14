2ae545e18907fc57921f35cc052dc910e79a492f
Mark Maxwell <1mar.max1@gmail.com>
2024-02-14 13:59:31 -0800

    refactor JSX to highlight useRef advantages.

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,48 @@
 // JSX AudioBufferSourceNode: Example 03
-// Same as previous, but with minor tweaks.
+// Buffer is created on first render only and stored as a ref.
+// AudioContext, playAudio, and stopAudio are also stored as refs 
+// to avoid disconnection through re-rendering of the component.
 
-const ctx = new AudioContext();
-let playAudio;
-let stopAudio;
-let myBuffer;
-
-fetch("MM_synth_fill_002.wav")
-  .then((res) => res.arrayBuffer())
-  .then((buffer) => ctx.decodeAudioData(buffer))
-  .then((buffer) => {
-    myBuffer = buffer;
-  });
-
-function createSourceNode() {
-  const mySourceNode = ctx.createBufferSource();
-  mySourceNode.buffer = myBuffer;
-  mySourceNode.connect(ctx.destination);
-  playAudio = () => mySourceNode.start();
-  stopAudio = () => mySourceNode.stop();
-}
+import { useRef, useEffect } from "react";
 
 function App() {
+  const ctx = useRef(new AudioContext());
+  const myBuffer = useRef();
+  const playAudio = useRef();
+  const stopAudio = useRef();
+
+  useEffect(() => {
+    fetch("MM_synth_fill_002.wav")
+      .then((res) => res.arrayBuffer())
+      .then((buffer) => ctx.current.decodeAudioData(buffer))
+      .then((buffer) => {
+        myBuffer.current = buffer;
+      });
+  }, []);
+
+  function createSourceNode() {
+    const mySourceNode = ctx.current.createBufferSource();
+    mySourceNode.buffer = myBuffer.current;
+    mySourceNode.connect(ctx.current.destination);
+    playAudio.current = () => mySourceNode.start();
+    stopAudio.current = () => mySourceNode.stop();
+  }
+
   return (
     <>
       <div>
-        <button id="play-btn"
+        <button
+          id="play-btn"
           onClick={() => {
             createSourceNode();
-            playAudio();
+            playAudio.current();
           }}
         >
           Play
         </button>
-        <button id="stop-btn" 
-          onClick={() => stopAudio()}
-        >
+        <button 
+          id="stop-btn" 
+          onClick={() => stopAudio.current()}>
           Stop
         </button>
       </div>
